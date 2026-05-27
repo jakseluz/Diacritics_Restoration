b9be7dd6d247ad380325d618c47f08304cf41169
jakseluz <labuzjak@gmail.com>
2026-05-27 17:03:02 +0200

    Fix evaluate_restorer_on_articles() method - DER calculating if UNK or PAD characters are being predicted

--- src/diacritics_restoration/utils/testing.py
+++ src/diacritics_restoration/utils/testing.py
@@ -21,22 +21,46 @@ def evaluate_restorer_on_articles(restorer: DiacriticsRestorer, num_articles=100
     pred_texts = decode_batch(dataset.processor, pred_ids[:3])
     true_texts = decode_batch(dataset.processor, true_ids[:3])
 
+    diacritics = set("ąćęłńóśźżĄĆĘŁŃÓŚŹŻ")
+    all_diacritics = 0
+    differences_diacritics = 0
+
     for i, (predicted, expected) in enumerate(zip(pred_texts, true_texts)):
-        differences = sum(1 for p, t in zip(predicted, expected) if p != t)
+        differences_char = sum(1 for p, t in zip(predicted, expected) if p != t)
+        differences_diacritics += sum(
+            1 for p, t in zip(predicted, expected) if p != t and t in diacritics
+        )
+        all_diacritics += sum(1 for t in expected if t in diacritics)
         print(f"\nExample {i}")
         print("PRED:", predicted)
         print("TRUE:", expected)
-        print(f"Differences: {differences} / {len(expected)}")
+        print(f"Differences: {differences_char} / {len(expected)}")
+        print(
+            f"Differences (diacritics): {differences_diacritics} / {all_diacritics} (so far in all examples)"
+        )
 
     mask = true_ids != pad_id
     total_differences = (pred_ids[mask] != true_ids[mask]).sum().item()
-    total_chars = mask.sum().item()
-    general_differences = (
-        total_differences / total_chars if total_chars > 0 else 0.0
+    total_chars = int(mask.sum().item())
+    cer = total_differences / total_chars if total_chars > 0 else 0.0
+
+    pred_flat = dataset.processor.sequence_to_text(pred_ids[mask].tolist())
+    true_flat = dataset.processor.sequence_to_text(true_ids[mask].tolist())
+
+    is_diacritic = torch.zeros(dataset.processor.vocab_size, dtype=torch.bool)
+    for char in diacritics:
+        is_diacritic[dataset.processor.char_id[char]] = True
+
+    diacritic_mask = mask & is_diacritic[true_ids]
+    all_diacritics = int(diacritic_mask.sum().item())
+    differences_diacritics = int(
+        (pred_ids[diacritic_mask] != true_ids[diacritic_mask]).sum().item()
     )
+    der = differences_diacritics / all_diacritics if all_diacritics > 0 else 0.0
 
     print(
-        f"\nOverall Character Error Rate: {general_differences:.6f} ({total_differences} differences out of {total_chars} characters)"
+        f"\nOverall Character Error Rate: {cer:.6f} ({total_differences} differences out of {total_chars} characters)",
+        f"\nOverall Diacritics Error Rate: {der:.6f} ({differences_diacritics} differences out of {all_diacritics} diacritics)",
     )
 
 
